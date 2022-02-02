830d42d2132faa79c6746354ea919c29c6ccb990
RobertoZeppilli <robzep171@gmail.com>
2022-02-02 11:53:13 +0100

    pages, tailwind and api setting

---
+++ src/routes/about.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+    <title>Pokedex - About</title>
+</svelte:head>
+<h1>
+    About
+</h1>
+<p>A really cool svelte kit demo that uses the pokemon API to build a pokedex</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { pokemon } from '../stores/pokestore';
+
+	// console.log($pokemon);
+</script>
+
+<svelte:head>
+	<title>Pokedex</title>
+</svelte:head>
+
+<h1>SvelteKit Pokedex</h1>
+
+{#each $pokemon as poke (poke.id)}
+    <p>{poke.name}</p>
+{/each}
